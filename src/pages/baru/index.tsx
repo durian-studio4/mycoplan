4802de4470bc9c8b4f83f28404f8e0c921ac66c1
kuetabby <vansmuhammad@gmail.com>
2020-09-24 08:21:27 +0700

    commiting

--- src/pages/baru/index.tsx
+++ src/pages/baru/index.tsx
@@ -1,26 +1,60 @@
 import React, { useState } from 'react';
 import { Button, Card, Row, Input } from 'antd';
 import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
+import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
 import styles from './index.less';
 
 interface Props {}
 
 const ProfileComponent: React.FC<Props> = () => {
   const [value, setValue] = useState('');
+
+  const handleChange = (address: any) => {
+    setValue(address);
+  };
+
+  const handleSelect = (address: any) => {
+    geocodeByAddress(address)
+      .then((results) => getLatLng(results[0]))
+      .then((latLng) => console.log('Success', latLng))
+      .catch((error) => console.error('Error', error));
+  };
+
   console.log(value, 'value');
+
   return (
-    <div>
-      <Card>
-        <p>Test</p>
-        <GooglePlacesAutocomplete
-          apiKey={REACT_APP_ENV_GMAPS}
-          selectProps={{
-            value,
-            onChange: setValue,
-          }}
-        />
-      </Card>
-    </div>
+    <PlacesAutocomplete value={value} onChange={handleChange} onSelect={handleSelect}>
+      {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+        <div>
+          <input
+            {...getInputProps({
+              placeholder: 'Search Places ...',
+              className: 'location-search-input',
+            })}
+          />
+          <div className="autocomplete-dropdown-container">
+            {loading && <div>Loading...</div>}
+            {suggestions.map((suggestion) => {
+              const className = suggestion.active ? 'suggestion-item--active' : 'suggestion-item';
+              // inline style for demonstration purpose
+              const style = suggestion.active
+                ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                : { backgroundColor: '#ffffff', cursor: 'pointer' };
+              return (
+                <div
+                  {...getSuggestionItemProps(suggestion, {
+                    className,
+                    style,
+                  })}
+                >
+                  <span>{suggestion.description}</span>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      )}
+    </PlacesAutocomplete>
   );
 };
 
