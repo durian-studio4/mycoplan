a70a1df7f06936d37da02715f96bbd3429aec055
kuetabby <vansmuhammad@gmail.com>
2020-11-27 22:11:02 +0700

    commit

--- src/pages/pesanan/TabsDelivery.tsx
+++ src/pages/pesanan/TabsDelivery.tsx
@@ -22,6 +22,7 @@ export const TabsDelivery: React.FC<Props> = ({
         <TabPane tab={`Menunggu Pembayaran (${data_jumlah_delivery(1).length || 0})`} key="1" />
         <TabPane tab={`Menunggu Konfirmasi (${data_jumlah_delivery(2).length || 0})`} key="2" />
         <TabPane tab={`Dalam Proses (${data_jumlah_delivery(3).length || 0})`} key="3" />
+        <TabPane tab={`Pengajuan Pembatalan (${data_jumlah_delivery(10).length || 0})`} key="10" />
         <TabPane tab={`Pengiriman (${data_jumlah_delivery(4).length || 0})`} key="4">
           <Tabs defaultActiveKey="pengambilan" onChange={onChangeStatusPengiriman}>
             <TabPane tab="Request Delivery Ulang" key="dikirim" />
@@ -35,7 +36,6 @@ export const TabsDelivery: React.FC<Props> = ({
         <TabPane tab={`Selesai (${data_jumlah_delivery(7).length || 0})`} key="7" />
         <TabPane tab={`Batal (${data_jumlah_delivery(8).length || 0})`} key="8" />
         <TabPane tab={`Expired (${data_jumlah_delivery(9).length || 0})`} key="9" />
-        <TabPane tab={`Pengajuan Pembatalan (${data_jumlah_delivery(10).length || 0})`} key="10" />
       </Tabs>
       {children}
     </>
--- src/pages/pesanan/TabsPickUp.tsx
+++ src/pages/pesanan/TabsPickUp.tsx
@@ -17,12 +17,12 @@ export const TabsPickUp: React.FC<Props> = ({ children, data_jumlah_pick_up, onC
         <TabPane tab={`Menunggu Pembayaran (${data_jumlah_pick_up(1).length || 0})`} key="1" />
         <TabPane tab={`Menunggu Konfirmasi (${data_jumlah_pick_up(2).length || 0})`} key="2" />
         <TabPane tab={`Dalam Proses (${data_jumlah_pick_up(3).length || 0})`} key="3" />
+        <TabPane tab={`Pengajuan Pembatalan (${data_jumlah_pick_up(10).length || 0})`} key="10" />
         <TabPane tab={`Menunggu Pick Up (${data_jumlah_pick_up(5).length || 0})`} key="5" />
         <TabPane tab={`Penyesuaian (${data_jumlah_pick_up(6).length || 0})`} key="6" />
         <TabPane tab={`Selesai (${data_jumlah_pick_up(7).length || 0})`} key="7" />
         <TabPane tab={`Batal (${data_jumlah_pick_up(8).length || 0})`} key="8" />
         <TabPane tab={`Expired (${data_jumlah_pick_up(9).length || 0})`} key="9" />
-        <TabPane tab={`Pengajuan Pembatalan (${data_jumlah_pick_up(10).length || 0})`} key="10" />
       </Tabs>
       {children}
     </>
